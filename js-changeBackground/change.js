18cdc1da251b6eafbab120d6bf4fb26148243958
karatosun8 <sefa_karatosun@hotmail.com>
2023-06-02 15:38:15 +0300

    finish

---
+++ js-changeBackground/change.js
@@ -0,0 +1,22 @@
+console.log("Merhaba Dünya");
+const btnClick =document.getElementById("change")
+const btnOver =document.getElementById("over")
+let text =document.querySelector("#text")
+const colorPrint =document.createElement("p")
+text.appendChild(colorPrint)
+
+btnClick.onclick= () => {
+    let color1 = Math.trunc(Math.random()*255);
+    let color2 = Math.trunc(Math.random()*255);
+    let color3 = Math.trunc(Math.random()*255);
+    document.body.style.backgroundColor = "rgb("+color1+","+color2+","+color3+")";
+}
+
+btnOver.onmouseover = () =>{
+    let color1 = Math.trunc(Math.random()*255);
+    let color2 = Math.trunc(Math.random()*255);
+    let color3 = Math.trunc(Math.random()*255);
+    document.body.style.backgroundColor = "rgb("+color1+","+color2+","+color3+")"; 
+    text.innerHTML = document.body.style.backgroundColor;
+   
+}
