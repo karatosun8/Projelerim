b2d7bbec5c99f16a0fe3ba9ec26d9980a30d9e02
karatosun8 <sefa_karatosun@hotmail.com>
2023-06-16 16:35:35 +0300

    start

---
+++ To-Do-App/project-1/app.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector("input")
+
+
+
+
+// document.querySelectorAll(".checkbox").addEventListener("click",function(){
+//   document.getElementById("card").style.backgroundColor="#8160809c"
+
+// })
+btn.addEventListener("click",()=>{
+   
+  document.getElementById("card").style.backgroundColor="#6200990c"
+
+})
+
+
