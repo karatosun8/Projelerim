3f4c71067ad85daf8af04a889db5fbc20c83063e
karatosun8 <sefa_karatosun@hotmail.com>
2023-06-16 17:10:34 +0300

    finish

--- To-Do-App/increase-decrease/app.js
+++ To-Do-App/increase-decrease/app.js
@@ -5,14 +5,14 @@ const number =document.querySelector("#number")
 console.log(number);
 let initialNumber =0;
 //butonlar btıklanmış mı dinle
+//eğer tıklanırsa artır
 increaseButton.addEventListener("click",() =>{
     initialNumber++;
     number.innerHTML =`<span>${initialNumber}</span>`;
 });
-
+//eğer tıklanırsa azalt
 decreaseButton.addEventListener("click",()=>{
     initialNumber--;
     number.innerHTML=`<span> ${initialNumber} </span>`
 })
-//eğer tıklanırsa artır ya da azalt
-//Title'yi bul ve onu artır veya azalt
+ 
