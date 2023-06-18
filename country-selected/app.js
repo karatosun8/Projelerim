65c3b1b763ca8ba2811731759b865bd03c3cc414
karatosun8 <sefa_karatosun@hotmail.com>
2023-06-18 19:42:50 +0300

    finish

--- country-selected/app.js
+++ country-selected/app.js
@@ -3,18 +3,18 @@ let countryInp = document.getElementById("country-inp")
 searchBtn.addEventListener("click", ()=>{
     let countryName = countryInp.value;
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    console.log(finalURL);
+    // console.log(finalURL);
     fetch(finalURL).then((res) => res.json())
     .then((data) => {
-        console.log(data[0]);
-        console.log(data[0].capital[0]);
-        console.log(data[0].flags.svg);
-        console.log(data[0].name.common);
-        console.log(data[0].continents[0]);
-        console.log(Object.keys(data[0].currencies)[0]);
-        console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-        console.log(Object.values(data[0].languages).toString().split(",").join(","));
-        countryInp.value=""
+        // console.log(data[0]);
+        // console.log(data[0].capital[0]);
+        // console.log(data[0].flags.svg);
+        // console.log(data[0].name.common);
+        // console.log(data[0].continents[0]);
+        // console.log(Object.keys(data[0].currencies)[0]);
+        // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+        // console.log(Object.values(data[0].languages).toString().split(",").join(","));
+        
         result.innerHTML =`
         <img src="${data[0].flags.svg}"
         class="flag-img">
@@ -59,4 +59,5 @@ searchBtn.addEventListener("click", ()=>{
         }
         
     })
+    
 })
