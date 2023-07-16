3cbc7768ba950c38ce75703b6f01927b6232985a
karatosun8 <sefa_karatosun@hotmail.com>
2023-07-16 11:36:41 +0300

    otomatik date

--- appointment-app-hospital/src/components/AddModal.jsx
+++ appointment-app-hospital/src/components/AddModal.jsx
@@ -1,48 +1,51 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
+function AddModal({ show, handleClose }) {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState(new Date().toISOString().slice(0,10));  // tarihin otomatik olarak gelmesi için
+ 
+  console.log(name,date);
 
-
-function AddModal({show,handleClose}) {
-    
-    return (
-      <>
-        
-  
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Appointment for...</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Appointment for...</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
           <Form>
-      <Form.Group className="mb-3" controlId="name">
-        <Form.Label>Patient Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter Your Name" />
-        
-      </Form.Group>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Your Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="date">
-        <Form.Label>Day & Time</Form.Label>
-        <Form.Control type="date" placeholder="Password" />
-      </Form.Group>
-      
-      <div className='text-center'>
-      <Button variant="primary" type="submit" className='me-2'>
-        Save
-      </Button>
-      <Button variant="secondary" onClick={handleClose}>
-        Close
-      </Button>
-      </div>
-    </Form>
+            <Form.Group className="mb-3" controlId="date">
+              <Form.Label>Day & Time</Form.Label>
+              <Form.Control type="date" placeholder="Password"
+              onChange={(e)=>setDate(e.target.value)} 
+              value={date}/>
+            </Form.Group>
 
-          </Modal.Body>
-          
-        </Modal>
-      </>
-    );
-  }
-  
-  export default AddModal;
+            <div className="text-center">
+              <Button variant="primary" type="submit" className="me-2">
+                Save
+              </Button>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default AddModal;
