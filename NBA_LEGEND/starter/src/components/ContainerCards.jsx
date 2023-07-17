d994aea3441ced0f2de7118097c488c08cdceaff
karatosun8 <sefa_karatosun@hotmail.com>
2023-07-17 11:23:29 +0300

    cardContainer

--- NBA_LEGEND/starter/src/components/ContainerCards.jsx
+++ NBA_LEGEND/starter/src/components/ContainerCards.jsx
@@ -1,52 +1,19 @@
-import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { data } from "../helpers/data";
-import PlayerCard from "./PlayerCard";
 
-const ContainerCards = () => {
-  console.log(data);
-  const [search, setSearch] = useState("");
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setSearch(e.target.value);
-  };
-  const filteredData = data.filter((item) =>
-    item.name.toLowerCase().includes(search.trim().toLowerCase())
-  );
-  console.log(filteredData);
+const ContainerCard = () => {
   return (
     <>
-      <Form.Control
-        type="search"
-        placeholder="Search Player..."
-        onChange={handleChange}
-      />
-      <Container className="p-3 rounded-4 card-container my-3">
-        <Row xs={2} md={4} lg={6} className="justify-content-center g-3">
-          
-          {/* aşağıdaki şekilde olabilir */}
-          {/* {filteredData.map((player, i) => (
-            <PlayerCard
-              key={i}
-              player={player}
-              img={player.img}
-              name={player.name}
-              statistics={player.statistics}
-            />
-          ))} */}
-          {filteredData.map((player, i) => (
-            <PlayerCard
-              key={i}
-            //   objenin içerisindeki tüm veriler lazımsa {...player kullanılır}
-              {...player}
-            />
-          ))}
+      <Form.Control type="search" placeholder="Search Player..." />
+      <Container className="card-container my-3 rounded-2">
+        <Row>
+          <Col>1 of 1</Col>
         </Row>
       </Container>
     </>
   );
 };
-export default ContainerCards;
+
+export default ContainerCard;
--- NBA_LEGEND/starter/src/components/Header.jsx
+++ NBA_LEGEND/starter/src/components/Header.jsx
@@ -1,14 +1,23 @@
+import logo from "../assets/nba-logo.png";
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
-import logo from "../assets/nba-logo.png"
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Header =()=>{
-
     return(
-        <Container >
-            <Image src={logo} width="200px"/>
-            <h1 className='my-2'>NBA LEGENDS</h1>
-        </Container>
+
+        <Container>
+      <Row className="justify-content-center">
+        <Col><img src={logo} width={"200px"}/></Col>
+        
+      </Row>
+      <Row>
+      <Col className="my-3"><h2>NBA LEGEND</h2> </Col>
+      </Row>
+      
+    </Container>
+        
+
     )
 }
 
