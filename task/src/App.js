7cf4bcb54991044609f10862c0cbbb5756cfa07c
karatosun8 <sefa_karatosun@hotmail.com>
2023-07-28 15:42:06 +0300

    görev ekleme

--- task/src/App.js
+++ task/src/App.js
@@ -4,11 +4,14 @@ import TaskCreate from './components/TaskCreate';
 import TaskList from './components/TaskList';
 
 function App() {
+  const createTask =(title,taskDesc)=>{
+    
+}
   return (
     <div className="App">
-      <TaskCreate/>
+      <TaskCreate onCreate={createTask}/> {/*onCreate yakalandı*/}
       <h1>Görevler</h1>
-      <TaskList/>
+      <TaskList />
     </div>
   );
 }
--- task/src/components/TaskCreate.js
+++ task/src/components/TaskCreate.js
@@ -1,15 +1,33 @@
+import {useState} from "react"
 
+function TaskCreate ({onCreate}){//onCreate App.js e gönderildi
+    const [title,setTitle] = useState("")
+    const [taskDesc,setTaskDesc] = useState("")
 
-function TaskCreate (){
+    const handleChange =(event)=>{
+        setTitle(event.target.value)
+
+    }
+    const handleTaskChange =(event)=>{
+        setTaskDesc(event.target.value)
+
+    }
+    const handleSubmit =(event)=>{
+        event.preventDefault();
+        onCreate(title,taskDesc);//title ve taskCreate App.js e gönderdik
+        setTitle("");//handleSubmitten sonra input boşalır
+        setTaskDesc("")//handleSubmitten sonra input boşalır
+
+    };
     return (
         <div className="task-create">
             <h3>Lütfen Task Ekleyiniz</h3>
             <form className="task-form">
                 <label className="task-label">Başlık</label>
-                <input className="task-input"/>
+                <input value={title} onChange={handleChange} className="task-input"/>
                 <label className="task-label">Task Giriniz</label>
-                <textarea className="task-input" rows={5}/>
-                <button className="task-button">Oluştur</button>
+                <textarea value={taskDesc} onChange={handleTaskChange} className="task-input" rows={5}/>
+                <button className="task-button" onClick={handleSubmit}>Oluştur</button>
             </form>
         </div>
     )
