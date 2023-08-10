c1e835298a8b6a24d1d4997f92f1d8273fba32ff
karatosun8 <sefa_karatosun@hotmail.com>
2023-08-10 13:43:25 +0300

    burger router

---
+++ burger/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import "../style/Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='socialMedia'>
+            <FacebookIcon/>
+            <InstagramIcon/>
+            <TwitterIcon/>
+        </div>
+        <p>Tüm Hakları Saklıdır | BurgerYiyelim</p>
+    </div>
+  )
+}
+
+export default Footer
